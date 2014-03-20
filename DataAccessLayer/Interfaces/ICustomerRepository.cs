7a54928831322490bb2365d22b894d8173b645fe
alexed1 <alex@edelstein.org>
2014-03-19 20:54:48 -0700

    merged in some SH-9 changes

---
+++ DataAccessLayer/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+using Shnexy.DataAccessLayer.Interfaces;
+using Shnexy.Models;
+
+namespace Shnexy.DataAccessLayer.Repositories
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        
+    }
+}
