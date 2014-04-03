27d2a83017c83d56dba77d78759a3f372df45fd4
alexed1 <alex@edelstein.org>
2014-04-03 12:18:42 -0700

    more work to get packages working correctly

--- Utilities/PersistableIntCollection.cs
+++ Utilities/PersistableIntCollection.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
+//using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
