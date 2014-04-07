b63102f785829627d63019715f01ad912642e398
alexed1 <alex@edelstein.org>
2014-04-07 15:04:21 -0700

    improvements to home page content

---
+++ Data/DataAccessLayer/Fixtures/FixtureData - Customer.cs
@@ -0,0 +1,30 @@
+﻿using Data.Models;
+
+
+namespace Data.DataAccessLayer.Fixtures
+{
+    public partial class FixtureData
+    {
+
+
+
+
+       
+        public Customer TestCustomer()
+        {
+
+            return new Customer(customerRepo)
+            {
+                Id = 1,
+                FirstName = "Jack",
+                LastName = "Maginot",
+                emailAddr = TestEmailAddress1()
+
+            };
+        }
+
+
+
+    }
+}
+
