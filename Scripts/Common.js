1f0ebacf88a95a405bdd388ee803f08bc2e86f03
quadermamun1 <quader.mamun@gmail.com>
2014-04-09 06:55:31 +0600

    Delete, Move, Edit

---
+++ Scripts/Common.js
@@ -0,0 +1,47 @@
+﻿
+var CONTROLLER_NAME = 'Calendar';
+
+function getConfigFilePath(){
+    var tempPath = ''
+    tempPath = $(location).attr('href');    
+
+    arrPath = tempPath.split('//');
+    //tempPath = arrPath[0] + '//' + arrPath[1].substring(0, arrPath[1].indexOf('/'));
+
+    tempPath = arrPath[0] + '//' + arrPath[1].substring(0, arrPath[1].indexOf('/')) + '/' + CONTROLLER_NAME;
+
+    return tempPath
+}
+
+function getConfiguration() {
+
+    var configFilePath = getConfigFilePath();
+
+    var retValue = '';
+
+    $.get(configFilePath,
+        function (txt, status, jqXHR) {
+            $("#txtConfigValue").val(txt);
+        },
+        "text"
+    );   
+
+    return retValue;
+}
+
+function getURL(key) {
+    var configValue = $("#txtConfigValue").val();
+
+    var retValue = '';
+    var lines = configValue.split("\n");
+    
+    for (var i = 0, len = lines.length; i < len; i++) {
+        var arrLine = lines[i].split('=');        
+        if ($.trim(arrLine[0]) == $.trim(key)) {
+            retValue = arrLine[1];
+            break;
+        }
+    }
+
+    return retValue;
+}
