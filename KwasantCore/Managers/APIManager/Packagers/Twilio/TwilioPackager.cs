23f694879ae34d7f9c2aef5aec15943045b4f537
Robert Rudman <RJRudman@gmail.com>
2014-05-01 14:04:35 +0400

    KW-74

---
+++ KwasantCore/Managers/APIManager/Packagers/Twilio/TwilioPackager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using Twilio;
+
+namespace KwasantCore.Managers.APIManager.Packagers.Twilio
+{
+    public class TwilioPackager
+    {
+        private const string AccountSIDWebConfigName = "TwilioAccountSID";
+        private const string AuthTokenWebConfigName = "TwilioAuthToken";
+        private const string FromNumberWebConfigName = "TwilioFromNumber";
+
+        private readonly TwilioRestClient _twilio;
+        private readonly String _twilioFromNumber;
+        public TwilioPackager()
+        {
+            var accountSID = ConfigurationManager.AppSettings[AccountSIDWebConfigName];
+            var accountAuthKey = ConfigurationManager.AppSettings[AuthTokenWebConfigName];
+            _twilioFromNumber = ConfigurationManager.AppSettings[FromNumberWebConfigName];
+
+            if (String.IsNullOrEmpty(accountSID))
+                throw new ArgumentNullException(AccountSIDWebConfigName, "Value must be set in web.config");
+
+            if (String.IsNullOrEmpty(accountAuthKey))
+                throw new ArgumentNullException(AuthTokenWebConfigName, "Value must be set in web.config");
+
+            if (String.IsNullOrEmpty(_twilioFromNumber))
+                throw new ArgumentNullException(FromNumberWebConfigName, "Value must be set in web.config");
+
+            _twilio = new TwilioRestClient(accountSID, accountAuthKey);
+        }
+
+        public void SendSMS(String number, String message)
+        {
+            _twilio.SendSmsMessage(_twilioFromNumber, number, message);
+        }
+    }
+}
