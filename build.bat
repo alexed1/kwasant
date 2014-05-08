fb39a774d967c07c6f64da71d7da7789048e0c41
Robert Rudman <RJRudman@gmail.com>
2014-05-08 10:33:10 +0400

    Build

--- build.bat
+++ build.bat
@@ -1,10 +1,12 @@
 @echo off
 
+git clean -fdx
+if not "%ERRORLEVEL%" == "0" goto :ERROR
 ".nuget/nuget.exe" restore Kwasant.sln
 if not "%ERRORLEVEL%" == "0" goto :ERROR
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" Kwasant.sln
 if not "%ERRORLEVEL%" == "0" goto :ERROR
-"packages\NUnit.Runners.2.6.3\tools\nunit-console.exe" Tests/KwasantTest/KwasantTest.csproj
+"packages\NUnit.Runners.2.6.3\tools\nunit-console.exe" -labels Tests/KwasantTest/KwasantTest.csproj
 if not "%ERRORLEVEL%" == "0" goto :ERROR
 
 exit /B 0
