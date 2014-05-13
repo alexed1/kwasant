ff71d48a189a8a598a99d1808d77f3e5cb1e4169
pabitrad <pabitrad@yahoo.com>
2014-05-13 16:19:17 +0530

    test case files

---
+++ Tests/KwasantTest/Fixtures/FixtureData - Person.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+using NUnit.Framework;
+
+namespace KwasantTest.Fixtures
+{
+    partial class FixtureData
+    {
+        [Test]
+        public PersonDO TestPerson()
+        {
+            return new PersonDO()
+            {
+                FirstName = "Pabitra",
+                EmailAddress = TestEmail3()
+            };
+        }
+    }
+}
