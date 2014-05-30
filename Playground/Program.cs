0d6cb520989fa0ddb3165784a59b544dbfec74f4
Robert Rudman <RJRudman@gmail.com>
2014-05-30 14:30:44 +0400

    KW-135 Merge fixes

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -23,50 +23,6 @@ namespace Playground
 
             KwasantDbContext db = new KwasantDbContext();
             db.Database.Initialize(true);
-
-            IUnitOfWork unitOfWork = ObjectFactory.GetInstance<IUnitOfWork>();
-            var evDO = new EventDO()
-            {
-            };
-            evDO.Attendees = new List<AttendeeDO>
-            {
-                new AttendeeDO
-                {
-                    EmailAddress = "alex@edelstein.org",
-                    EventID = evDO.Id,
-                    Event = evDO,
-                    Name = "Alex Edelstein",
-               },
-                               new AttendeeDO
-                {
-                    EmailAddress = "alexed@yahoo.com",
-                    EventID = evDO.EventID,
-                    Event = evDO,
-                    Name = "Katie Vanderdrift",
-
-               },        
-               new AttendeeDO
-                {
-                    EmailAddress = "kwasanttest@icloud.com",
-                    EventID = evDO.EventID,
-                    Event = evDO,
-                    Name = "KwasantTest",
-               },
-               //new AttendeeDO
-               // {
-               //     EmailAddress = "alex@edelstein.org",
-               //     EventID = evDO.EventID,
-               //     Event = evDO,
-               //     Name = "Alex Edelstein",
-               //},
-            };
-            evDO.Location = "Skype";
-            evDO.Description = "Discuss event visualizations";
-            evDO.Summary = "aaaaa Gmail and outlook works";
-            evDO.StartDate = DateTime.Now.AddHours(3);
-            evDO.EndDate = DateTime.Now.AddHours(4);
-
-            new Event().Dispatch(evDO);
         }
     }
 }
