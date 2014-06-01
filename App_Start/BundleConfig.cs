8297b98fce35eec84440a32c9543fc4eb2ef2884
Kate <eschebenyuk@gmail.com>
2014-06-01 23:59:24 +0300

    Fixes for home page

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -76,6 +76,9 @@ namespace KwasantWeb.App_Start
             bundles.Add(new StyleBundle("~/bundles/css/bootstrap3.0").Include(
                 "~/Content/css/bootstrap3.0.css"
                 ));
+			bundles.Add(new StyleBundle("~/bundles/css/bootstrap-responsive").Include(
+				"~/Content/css/bootstrap-responsive.css"
+				));
             bundles.Add(new StyleBundle("~/bundles/css/colorbox").Include(
                 "~/Content/css/colorbox.css"
                 ));
