95855ff9e0f624fb5a398d71a1d9bc99e5f82375
Robert Rudman <RJRudman@gmail.com>
2014-06-02 12:25:31 +0400

    Fix route again

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -11,8 +11,9 @@ namespace KwasantWeb.App_Start
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}"
-            ); ;
+                url: "{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional}
+                );
         }
     }
 }
