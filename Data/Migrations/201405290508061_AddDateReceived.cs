b74497ed1f21e3b309900080b140f5b889c2dcf8
Robert Rudman <RJRudman@gmail.com>
2014-06-02 11:27:35 +0400

    Fix build

---
+++ Data/Migrations/201405290508061_AddDateReceived.cs
@@ -0,0 +1,18 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDateReceived : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Emails", "DateReceived", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Emails", "DateReceived");
+        }
+    }
+}
