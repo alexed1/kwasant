8a6792b937a8eec5f8f7b0196fc0fb40f600cf45
alexed <alex@edelstein.org>
2014-06-06 14:20:13 -0700

    event description can now be longer

---
+++ Data/Validations/EventValidator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data.Entities;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Data.Validators
+{
+    public class EventValidator : AbstractValidator<EventDO>
+    {
+        public EventValidator()
+        {
+            RuleFor(eventDO => eventDO.StartDate)
+                .NotEmpty()
+                .WithMessage("Start Date is Required");
+
+            RuleFor(eventDO => eventDO.EndDate)
+                .NotEmpty().WithMessage("End date is required")
+                .GreaterThanOrEqualTo(eventDO => eventDO.StartDate)
+                .WithMessage("End date must after Start date");
+
+            RuleFor(eventDO => eventDO.Description).NotNull().SetValidator(new StringMinLength(3));
+
+            RuleFor(eventDO => eventDO.Attendees).SetValidator(new ListMustContainAtLeastOneItem<AttendeeDO>())
+                .WithMessage("Event must have at least one attendee");
+
+            RuleFor(eventDO => eventDO.CreatedBy)
+                .NotNull()
+                .WithMessage("CreatedBy is Required");
+
+        }
+
+        //=================================================================
+        //Utilities 
+        //TO DO: Genericize this
+        public void ValidateEvent(EventDO curEventDO)
+        {
+            ValidationResult results = Validate(curEventDO);
+            if (results.IsValid)
+                return;
+
+            throw new ValidationException(results.Errors);
+
+        }
+    }
+}
