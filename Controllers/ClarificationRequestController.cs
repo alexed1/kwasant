d68759cdb7843654c40f7b4d1651c835d00ed401
Max Kostyrkin <mkostyrkin@gmail.com>
2014-06-14 12:01:02 +0300

    ViewModel, Controller

---
+++ Controllers/ClarificationRequestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KwasantCore.Managers.IdentityManager;
+using KwasantWeb.ViewModels;
+
+namespace KwasantWeb.Controllers
+{
+    [KwasantAuthorize(Roles = "Admin")]
+    public class ClarificationRequestController : Controller
+    {
+        public ActionResult Edit(int bookingRequestId)
+        {
+            return View(new ClarificationRequestViewModel());
+        }
+    }
+}
---
+++ ViewModels/ClarificationRequestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KwasantWeb.ViewModels
+{
+    public class ClarificationRequestViewModel
+    {
+    }
+}
