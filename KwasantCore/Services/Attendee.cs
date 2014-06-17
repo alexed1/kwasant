42609015228a26ccdd6116c868f63059af1f591b
Robert Rudman <RJRudman@gmail.com>
2014-06-17 18:00:29 +0400

    KW-197 Email searching and tests

--- KwasantCore/Services/Attendee.cs
+++ KwasantCore/Services/Attendee.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Data.Entities;
 using Data.Interfaces;
 using StructureMap;
@@ -55,6 +57,65 @@ namespace KwasantCore.Services
                     Name = attendee
                 };
                 uow.AttendeeRepository.Add(newAttendee);
+            }
+        }
+
+        public void DetectFromBookingRequest(BookingRequestDO bookingRequestDO)
+        {
+            
+        }
+
+        public List<ParsedEmailAddress> GetEmailAddresses(String textToSearch)
+        {
+            //This is the email regex.
+            //It searches for emails in the format of <Some Person>somePerson@someDomain.someExtension
+
+            //We assume that names can only contain letters, numbers, and spaces. We also allow for a blank name, in the form of <>
+            const string nameRegex = @"[ a-zA-Z0-9]*";
+
+            //We assume for now, that emails can only contain letters and numbers. This can be updated in the future (parsing emails is actually incredibly difficult).
+            //See http://tools.ietf.org/html/rfc2822#section-3.4.1 in the future if we ever update this.
+            const string emailUserNameRegex = @"[a-zA-Z0-9]*";
+
+            //Domains can only contain letters or numbers.
+            const string domainRegex = @"[a-zA-Z0-9]+";
+
+            //Top level domain must be at least two characters long. Only allows letters.
+            const string tldRegex = @"[a-zA-Z]{2,}";
+
+            //The name part is optional; we can find emails like 'rjrudman@gmail.com', or '<Robert Rudman>rjrudman@gmail.com'.
+            //The regex uses named groups; 'name' and 'email'.
+            //Name will contain the name, without <>. Email will contain the full email address (without the name).
+
+            //Typically, you won't need to modify the below code, only the four variables defined above.
+            var fullRegexExpression = String.Format(@"(<(?<name>{0})>)?(?<email>{1}@{2}\.{3})", nameRegex, emailUserNameRegex, domainRegex, tldRegex);
+
+            var regex = new Regex(fullRegexExpression);
+
+            var result = new List<ParsedEmailAddress>();
+            foreach (Match match in regex.Matches(textToSearch))
+            {
+                var parse = new ParsedEmailAddress
+                {
+                    Name = match.Groups["name"].Value,
+                    Email = match.Groups["email"].Value
+                };
+                result.Add(parse);
+            }
+            return result;
+        }
+
+
+        public class ParsedEmailAddress
+        {
+            public String Name { get; set; }
+            public String Email { get; set; }
+
+            public override string ToString()
+            {
+                if (String.IsNullOrEmpty(Name))
+                    return Email;
+                return String.Format("<{0}>{1}", Name, Email);
             }
         }
     }
---
+++ Tests/KwasantTest/Services/AttendeeTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using KwasantCore.Services;
+using NUnit.Framework;
+
+namespace KwasantTest.Services
+{
+    [TestFixture]
+    public class AttendeeTests
+    {
+        [Test]
+        public void TestBasicEmail()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("rjrudman@gmail.com");
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(String.Empty, result[0].Name);
+            Assert.AreEqual("rjrudman@gmail.com", result[0].Email);
+        }
+
+        [Test]
+        public void TestMultipleBasicEmails()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("rjrudman@gmail.com,otheremail@gmail.com");
+
+            Assert.AreEqual(2, result.Count);
+
+            Assert.AreEqual(String.Empty, result[0].Name);
+            Assert.AreEqual("rjrudman@gmail.com", result[0].Email);
+
+            Assert.AreEqual(String.Empty, result[1].Name);
+            Assert.AreEqual("otheremail@gmail.com", result[1].Email);
+        }
+
+        [Test]
+        public void TestEmailWithName()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("<Robert>rjrudman@gmail.com");
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("Robert", result[0].Name);
+            Assert.AreEqual("rjrudman@gmail.com", result[0].Email);
+        }
+
+        [Test]
+        public void TestEmailWithFullName()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("<Robert Robert>rjrudman@gmail.com");
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("Robert Robert", result[0].Name);
+            Assert.AreEqual("rjrudman@gmail.com", result[0].Email);
+        }
+
+        [Test]
+        public void TestNameWithNumbers()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("<Robert23>rjrudman@gmail.com");
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("Robert23", result[0].Name);
+            Assert.AreEqual("rjrudman@gmail.com", result[0].Email);
+        }
+
+        [Test]
+        public void TestEmailNameWithNumbers()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("rjrudman23@gmail.com");
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(String.Empty, result[0].Name);
+            Assert.AreEqual("rjrudman23@gmail.com", result[0].Email);
+        }
+
+        [Test]
+        public void TestDomainlNameWithNumbers()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("rjrudman@g23mail.com");
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(String.Empty, result[0].Name);
+            Assert.AreEqual("rjrudman@g23mail.com", result[0].Email);
+        }
+
+        [Test]
+        public void TestInvalidTLD_Short()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("rjrudman@gmail.c");
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [Test]
+        public void TestInvalidTLD_Numbers()
+        {
+            var att = new Attendee();
+            var result = att.GetEmailAddresses("rjrudman@gmail.c24om");
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+    }
+}
