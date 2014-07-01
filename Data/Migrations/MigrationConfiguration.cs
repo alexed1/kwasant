f3cde6cb614df3e723e677ae19038d5b84ff64d9
Robert Rudman <RJRudman@gmail.com>
2014-07-01 15:19:12 +0400

    KW-246

--- Data/Migrations/MigrationConfiguration.cs
+++ Data/Migrations/MigrationConfiguration.cs
@@ -12,6 +12,7 @@ using Data.Constants;
 using Data.Entities;
 using Data.Infrastructure;
 using Data.Interfaces;
+using Utilities;
 
 namespace Data.Migrations
 {
@@ -180,15 +181,7 @@ namespace Data.Migrations
             }
             catch (DbEntityValidationException e)
             {
-                string errorFormat = @"Validation failed for entity [{0}]. Validation errors:" + Environment.NewLine + @"{1}";
-                var errorList = new List<String>();
-                foreach (var entityValidationError in e.EntityValidationErrors)
-                {
-                    var entityName = entityValidationError.Entry.Entity.GetType().Name;
-                    var errors = String.Join(Environment.NewLine, entityValidationError.ValidationErrors.Select(a => a.PropertyName + ": " + a.ErrorMessage));
-                    errorList.Add(String.Format(errorFormat, entityName, errors));
-                }
-                throw new Exception(String.Join(Environment.NewLine + Environment.NewLine, errorList) + Environment.NewLine, e);
+                ExceptionHandling.DisplayValidationErrors(e);
             }
         }
     }
