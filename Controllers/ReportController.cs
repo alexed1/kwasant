035e5b20f728eb937807ac1e9915636e2ca3ebba
Amit <a.tech0@gmail.com>
2014-07-11 20:41:24 +0530

    Usage Report updates

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Data.Interfaces;
+using KwasantCore.Managers.APIManager.Packagers.DataTable;
+using KwasantCore.Services;
+using StructureMap;
+using Utilities;
+
+namespace KwasantWeb.Controllers
+{
+    public class ReportController : Controller
+    {
+        private DataTablesPackager _datatables;
+        private Report _report;
+
+        public ReportController()
+        {
+            _datatables = new DataTablesPackager();
+            _report = new Report();
+        }
+
+        //
+        // GET: /Report/
+        public ActionResult Index(string type)
+        {
+            ViewBag.type = type;
+            switch (type)
+            {
+                case "usage" :
+                    ViewBag.Title = "Usage Report";
+                    break;
+                case "incident":
+                    ViewBag.Title = "Incident Report";
+                    break;
+            }
+            return View();
+        }
+
+        public ActionResult ShowReportView(string queryPeriod, string type)
+        {
+            DateRange dateRange = DateUtility.GenerateDateRange(queryPeriod);
+            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
+            {
+                var jsonResult = Json(_datatables.Pack(_report.Generate(uow, dateRange, type)), JsonRequestBehavior.AllowGet);
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+            //return View();
+        }
+	}
+}
---
+++ KwasantCore/Services/Report.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+using Data.Interfaces;
+using Utilities;
+
+namespace KwasantCore.Services
+{
+    public class Report
+    {
+        public object Generate(IUnitOfWork uow, DateRange dateRange, string type)
+        {
+            switch (type)
+            {
+                case "usage":
+                    return GenerateUsageReport(uow, dateRange);
+                //case "incident":
+            }
+            return this;
+        }
+        private List<FactDO> GenerateUsageReport(IUnitOfWork uow, DateRange dateRange)
+        {
+            return uow.FactRepository.GetAll().Where(e => e.CreateDate > dateRange.StartTime && e.CreateDate < dateRange.EndTime).ToList();
+        }
+    }
+}
