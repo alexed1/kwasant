c1bc7e24bc28602c5d9fb245c8fa1301efb00c4b
Robert Rudman <RJRudman@gmail.com>
2014-08-05 17:12:34 +0400

    KW-309 Fix deleting in general Add delete button to edit page

--- Data/Entities/AttendeeDO.cs
+++ Data/Entities/AttendeeDO.cs
@@ -9,19 +9,15 @@ namespace Data.Entities
     {
         [Key]
         public int Id { get; set; }
-        [ForeignKey("Event")]
-        public int EventID { get; set; }
-
+        
         public String Name { get; set; }
 
         [ForeignKey("EmailAddress")]
         public int EmailAddressID { get; set; }
         public virtual EmailAddressDO EmailAddress { get; set; }
 
+        [ForeignKey("Event")]
+        public int EventID { get; set; }
         public virtual EventDO Event { get; set; }
-
-        //[ForeignKey("Negotiation")]
-        //public int? NegotiationID { get; set; }
-        //public virtual NegotiationDO Negotiation { get; set; }
     }
 }
