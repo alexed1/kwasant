2e868925ff510f59bfeeb8b06ecad4fe378bb5eb
Amit <a.tech0@gmail.com>
2014-09-10 15:12:37 +0530

    Updated Code -kw382

--- KwasantCore/Services/EmailAddress.cs
+++ KwasantCore/Services/EmailAddress.cs
@@ -1,13 +1,38 @@
 ﻿using Data.Entities;
 using Data.Interfaces;
+using StructureMap;
+using System;
 using System.Net.Mail;
 namespace KwasantCore.Services
 {
-   public class EmailAddress
+    public class EmailAddress
     {
-       public EmailAddressDO ConvertFromMailAddress(IUnitOfWork uow, MailAddress address)
-       {
-           return uow.EmailAddressRepository.GetOrCreateEmailAddress(address.Address, address.DisplayName);
-       }
+        public EmailAddressDO ConvertFromMailAddress(IUnitOfWork uow, MailAddress address)
+        {
+            return uow.EmailAddressRepository.GetOrCreateEmailAddress(address.Address, address.DisplayName);
+        }
+
+        public EmailAddressDO ConvertFromString(string emailString, IUnitOfWork uow)
+        {
+            String email = string.Empty;
+            String name = string.Empty;
+            emailString = emailString.Replace("\"", string.Empty);
+            if (emailString.Contains("<"))
+            {
+                string[] parts = emailString.Split('<');
+                name = parts[0];
+                email = parts[1];
+                email = email.Replace(">", string.Empty);
+            }
+            else
+                email = emailString;
+
+            //using (var subUow = ObjectFactory.GetInstance<IUnitOfWork>())
+            //{
+            EmailAddressDO convertAddresFromString = uow.EmailAddressRepository.GetOrCreateEmailAddress(email, name);
+            return convertAddresFromString;
+           //}
+        }
+
     }
 }
