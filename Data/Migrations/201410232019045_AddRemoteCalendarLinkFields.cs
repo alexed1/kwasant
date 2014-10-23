5c971f9aca602f88a9f3ff62ff52a9f56b34c0a3
Max Kostyrkin <mkostyrkin@gmail.com>
2014-10-23 23:57:53 +0300

    KW-464 Additional migration actions

--- Data/Migrations/201410232019045_AddRemoteCalendarLinkFields.cs
+++ Data/Migrations/201410232019045_AddRemoteCalendarLinkFields.cs
@@ -7,6 +7,9 @@ namespace Data.Migrations
     {
         public override void Up()
         {
+            // need to delete all auth data as we require an additional OAuth scope since now
+            Sql("DELETE dbo.RemoteCalendarAuthData");
+            Sql("DELETE dbo.RemoteCalendarLinks");
             AddColumn("dbo.RemoteCalendarLinks", "RemoteCalendarHref", c => c.String());
             AddColumn("dbo.RemoteCalendarLinks", "IsDisabled", c => c.Boolean(nullable: false));
         }
