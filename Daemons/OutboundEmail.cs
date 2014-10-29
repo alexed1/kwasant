6b7b185b587992610b535bfe3fc5ea6b3b630d24
Max Kostyrkin <mkostyrkin@gmail.com>
2014-10-29 09:33:17 +0200

    KW-464 AlertReporter moved to KwasantCore, sends Emails.

--- Daemons/OutboundEmail.cs
+++ Daemons/OutboundEmail.cs
@@ -62,9 +62,7 @@ namespace Daemons
                         uow.SaveChanges();
                     }
 
-                    AlertManager.Error_EmailSendFailure();
-                    Email _email = ObjectFactory.GetInstance<Email>();
-                    _email.SendAlertEmail("Alert! Kwasant Error Reported: EmailSendFailure");
+                    AlertManager.Error_EmailSendFailure(emailID, message);
                 });
 
             AddTest("OutboundEmailDaemon_Test", "Test");
