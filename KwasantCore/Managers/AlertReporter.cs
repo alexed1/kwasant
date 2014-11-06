87affcdb46d0dd0c4b087c374f61b9ad17304a12
alexed <alex@edelstein.org>
2014-11-05 18:56:06 -0800

    move IncidentReporter to KwasantCore and consolidate all Incident reporting there

--- KwasantCore/Managers/AlertReporter.cs
+++ KwasantCore/Managers/AlertReporter.cs
@@ -10,6 +10,9 @@ using StructureMap;
 using Utilities;
 using Utilities.Logging;
 
+//NOTES: Do NOT put Incidents here. Put them in IncidentReporter
+
+
 namespace KwasantCore.Managers
 {
     public class AlertReporter
@@ -23,12 +26,12 @@ namespace KwasantCore.Managers
             AlertManager.AlertBookingRequestCreated += ProcessBookingRequestCreated;
             AlertManager.AlertBookingRequestStateChange += ProcessBookingRequestStateChange;
             AlertManager.AlertExplicitCustomerCreated += NewExplicitCustomerCreated;
-            AlertManager.AlertBookingRequestProcessingTimeout += ProcessTimeout;
+        
             AlertManager.AlertUserRegistration += UserRegistration;
             AlertManager.AlertBookingRequestCheckedOut += ProcessBookingRequestCheckedOut;
             AlertManager.AlertBookingRequestOwnershipChange += BookingRequestOwnershipChange;
-            AlertManager.AlertError_EmailSendFailure += Error_EmailSendFailure;
-            AlertManager.AlertErrorSyncingCalendar += ErrorSyncingCalendar;
+  
+            
             AlertManager.AlertPostResolutionNegotiationResponseReceived += OnPostResolutionNegotiationResponseReceived;
         }
 
@@ -131,7 +134,7 @@ namespace KwasantCore.Managers
         {
             FactDO curAction = new FactDO()
                 {
-                    Name = "EmailSent",
+                    Name = "",
                     PrimaryCategory = "Email",
                     SecondaryCategory = "",
                     Activity = "Sent",
@@ -149,7 +152,7 @@ namespace KwasantCore.Managers
                 var bookingRequestDO = uow.BookingRequestRepository.GetByKey(bookingRequestId);
                 FactDO curAction = new FactDO()
                     {
-                        Name = "BookingRequest Created",
+                        Name = "",
                         PrimaryCategory = "BookingRequest",
                         SecondaryCategory = "",
                         Activity = "Created",
@@ -212,22 +215,7 @@ namespace KwasantCore.Managers
                 Logger.GetLogger().Info(curAction.Data);
             uow.FactRepository.Add(curAction);
         }
-        public void ProcessTimeout(BookingRequestDO bookingRequestDO)
-        {
-            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
-            {
-                IncidentDO incidentDO = new IncidentDO();
-                incidentDO.PrimaryCategory = "Timeout";
-                incidentDO.SecondaryCategory = "BookingRequest";
-                incidentDO.CreateTime = DateTime.Now;
-                incidentDO.Activity = "";
-                incidentDO.ObjectId = bookingRequestDO.Id;
-                incidentDO.CustomerId = bookingRequestDO.User.Id;
-                incidentDO.BookerId = bookingRequestDO.UserID;
-                uow.IncidentRepository.Add(incidentDO);
-                uow.SaveChanges();
-            }
-        }
+
 
 
         public void UserRegistration(UserDO curUser)
@@ -236,7 +224,7 @@ namespace KwasantCore.Managers
             {
                 FactDO curFactDO = new FactDO
                     {
-                        Name = "UserRegistration Created",
+                        Name = "",
                         PrimaryCategory = "User",
                         SecondaryCategory = "",
                         Activity = "Registered",
@@ -252,57 +240,9 @@ namespace KwasantCore.Managers
 
         }
 
-        private void Error_EmailSendFailure(int emailId, string message)
-        {
-            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
-            {
-                IncidentDO incidentDO = new IncidentDO();
-                incidentDO.PrimaryCategory = "EmailFailure";
-                incidentDO.SecondaryCategory = "Email";
-                incidentDO.CreateTime = DateTime.Now; ;
-                incidentDO.Activity = "SendFailure";
-                incidentDO.ObjectId = emailId;
-                incidentDO.Notes = message;
-                uow.IncidentRepository.Add(incidentDO);
-                uow.SaveChanges();
-            }
-            Email _email = ObjectFactory.GetInstance<Email>();
-            _email.SendAlertEmail("Alert! Kwasant Error Reported: EmailSendFailure",
-                                  string.Format(
-                                      "EmailID: {0}\r\n" +
-                                      "Message: {1}",
-                                      emailId, message));
-        }
+       
 
-        private void ErrorSyncingCalendar(RemoteCalendarLinkDO calendarLink)
-        {
-            using (var uow = ObjectFactory.GetInstance<IUnitOfWork>())
-            {
-                IncidentDO incidentDO = new IncidentDO();
-                incidentDO.PrimaryCategory = "SyncFailure";
-                incidentDO.SecondaryCategory = "Calendar";
-                incidentDO.CreateTime = DateTime.Now;
-                incidentDO.Activity = "SyncFailure";
-                incidentDO.ObjectId = calendarLink.Id;
-                incidentDO.CustomerId = calendarLink.LocalCalendar.OwnerID;
-                incidentDO.Notes = calendarLink.LastSynchronizationResult;
-                uow.IncidentRepository.Add(incidentDO);
-                uow.SaveChanges();
-            }
-
-            Email email = ObjectFactory.GetInstance<Email>();
-            email.SendAlertEmail("CalendarSync failure",
-                                 string.Format(
-                                     "CalendarSync failure for calendar link #{0} ({1}):\r\n" +
-                                     "Customer id: {2},\r\n" +
-                                     "Local calendar id: {3}\r\n," +
-                                     "Remote calendar url: {4}",
-                                     calendarLink.Id,
-                                     calendarLink.LastSynchronizationResult,
-                                     calendarLink.LocalCalendar.OwnerID,
-                                     calendarLink.LocalCalendarID,
-                                     calendarLink.RemoteCalendarHref));
-        }
+      
 
         public void ProcessBookingRequestCheckedOut(int bookingRequestId, string bookerId)
         {
