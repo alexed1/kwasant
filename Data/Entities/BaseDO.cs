38d3bab60361103bfe213730e8cb4dab9af3f5b9
Max Kostyrkin <mkostyrkin@gmail.com>
2014-11-16 14:37:41 +0200

    KW-484 In progress

--- Data/Entities/BaseDO.cs
+++ Data/Entities/BaseDO.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using Data.Infrastructure;
 using Data.Interfaces;
+using Data.States.Templates;
+using Utilities;
 
 namespace Data.Entities
 {
-    public class BaseDO : IBaseDO
+    public class BaseDO : IBaseDO, IModifyHook
     {
         public DateTimeOffset LastUpdated { get; set; }
+
+        public void OnModify(DbPropertyValues originalValues, DbPropertyValues currentValues)
+        {
+            var idProperty = ReflectionHelper.EntityPrimaryKeyPropertyInfo(this);
+            var id = idProperty.GetValue(this);
+            var stateProperties = GetType().GetProperties()
+                .Where(p => p.PropertyType.GetInterfaces().Any(i => i == typeof (IStateTemplate<>)))
+                .ToArray();
+            foreach (var stateProperty in stateProperties)
+            {
+                if (!MiscUtils.AreEqual(originalValues[stateProperty.Name], currentValues[stateProperty.Name]))
+                {
+                    // AlertManager.TrackablePropertyUpdated("StateChange", "BookingRequest", id, uow.BookingRequestStatusRepository.GetByKey(State).Name);
+                }
+            }
+        }
     }
 }
