745c4d59dc1c82da3e47c2769796baaff224c9ec
Amit <a.tech0@gmail.com>
2014-12-02 14:40:49 +0530

    Revert "Merged Dev Into kw-588"
    This reverts commit 3042c77974ac77c6f608f626f9966aed3dd2e3eb.

--- KwasantCore/Services/Report.cs
+++ KwasantCore/Services/Report.cs
@@ -20,6 +20,7 @@ namespace KwasantCore.Services
             _email = new Email();
         }
 
+        public object Generate(IUnitOfWork uow, DateRange dateRange, string type)
         public object Generate(IUnitOfWork uow, DateRange dateRange, string type, int start,
             int length, out int recordcount)
         {
@@ -75,6 +76,9 @@ namespace KwasantCore.Services
             _dataUrlMappings.Add("Email", "/Dashboard/Index/");
             _dataUrlMappings.Add("User", "/User/Details?userID=");
 
+            return
+                uow.FactRepository.GetAll()
+                    .Where(e => e.CreateDate > dateRange.StartTime && e.CreateDate < dateRange.EndTime)
             var factDO = uow.FactRepository.GetAll()
                     .Where(e => e.CreateDate > dateRange.StartTime && e.CreateDate < dateRange.EndTime);
 
