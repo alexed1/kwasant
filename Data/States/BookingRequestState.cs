c9e22ede5df4b5167351d923b0c4f22426791d38
Robert Rudman <RJRudman@gmail.com>
2014-12-07 18:55:09 +0300

    Fix database with removal of 'Unstarted' BR state

--- Data/States/BookingRequestState.cs
+++ Data/States/BookingRequestState.cs
@@ -2,12 +2,11 @@
 {
     public class BookingRequestState
     {
-        public const int NeedsBooking = 1;
         public const int Resolved = 2;
         public const int Booking = 3;
         public const int Invalid = 4;
         public const int AwaitingClient = 5;
-        //public const int NeedsBooking = 6;
+        public const int NeedsBooking = 6;
         public const int Finished = 7;
     }
 }
