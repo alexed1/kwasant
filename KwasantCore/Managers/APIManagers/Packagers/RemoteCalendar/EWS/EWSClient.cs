285dc89e5b7bc0a06b55ec53b3844d26a75225ff
Max Kostyrkin <mkostyrkin@gmail.com>
2014-12-12 23:12:38 +0200

    KW-571 DDayEvent<->Appointment Adapter

--- KwasantCore/Managers/APIManagers/Packagers/RemoteCalendar/EWS/EWSClient.cs
+++ KwasantCore/Managers/APIManagers/Packagers/RemoteCalendar/EWS/EWSClient.cs
@@ -22,182 +22,74 @@ namespace KwasantCore.Managers.APIManagers.Packagers.RemoteCalendar.EWS
 {
     class EWSClient : IRemoteCalendarServiceClient
     {
-        class EWSEvent : IEvent
+        class EventToAppointmentAdapter
         {
-            private readonly Appointment _appointment;
-
-            public EWSEvent(Appointment appointment)
+            public IEvent AdaptAppointment(Appointment appointment)
             {
-                _appointment = appointment;
-                Name = _appointment.LastModifiedName;
-                Attendees = _appointment
-                    .RequiredAttendees.Union(_appointment.OptionalAttendees)
+                var e = new DDayEvent();
+                e.Attendees = appointment
+                    .RequiredAttendees.Union(appointment.OptionalAttendees)
                     .Select<Attendee, IAttendee>(a => new KwasantICS.DDay.iCal.DataTypes.Attendee(a.Address))
                     .ToList();
-                Categories = _appointment.Categories.ToList();
-                Summary = _appointment.Subject;
-                Organizer = new Organizer(_appointment.Organizer.Address) { CommonName = _appointment.Organizer.Name };
-                Start = new iCalDateTime(_appointment.Start, _appointment.StartTimeZone.Id);
-                End = new iCalDateTime(_appointment.End, _appointment.EndTimeZone.Id);
-                IsAllDay = _appointment.IsAllDayEvent;
-                Created = new iCalDateTime(_appointment.DateTimeCreated);
-                Location = _appointment.Location;
-                Description = _appointment.TextBody;
-                if (_appointment.ICalDateTimeStamp.HasValue)
+                e.Categories = appointment.Categories.ToList();
+                e.Summary = appointment.Subject;
+                e.Organizer = new Organizer(appointment.Organizer.Address) { CommonName = appointment.Organizer.Name };
+                e.Start = new iCalDateTime(appointment.Start, appointment.StartTimeZone.Id);
+                e.End = new iCalDateTime(appointment.End, appointment.EndTimeZone.Id);
+                e.IsAllDay = appointment.IsAllDayEvent;
+                e.Created = new iCalDateTime(appointment.DateTimeCreated);
+                e.Location = appointment.Location;
+                e.Description = appointment.TextBody;
+                if (appointment.ICalDateTimeStamp.HasValue)
                 {
-                    DTStamp = new iCalDateTime(_appointment.ICalDateTimeStamp.Value);
+                    e.DTStamp = new iCalDateTime(appointment.ICalDateTimeStamp.Value);
                 }
-                UID = _appointment.ICalUid;
-                Url = new Uri(_appointment.NetShowUrl);
+                e.UID = appointment.ICalUid;
+                e.Url = new Uri(appointment.NetShowUrl);
+                return e;
             }
 
-            public event EventHandler<ObjectEventArgs<string, string>> GroupChanged;
-            public string Group { get; set; }
-            public bool IsLoaded { get; private set; }
-            public event EventHandler Loaded;
-            public void OnLoaded()
+            public Appointment AdaptEvent(IEvent e, ExchangeService exchangeService)
             {
-                throw new NotImplementedException();
-            }
-
-            public void CopyFrom(ICopyable obj)
-            {
-                throw new NotImplementedException();
-            }
-
-            public T Copy<T>()
-            {
-                throw new NotImplementedException();
-            }
-
-            public object GetService(Type serviceType)
-            {
-                throw new NotImplementedException();
-            }
-
-            public object GetService(string name)
-            {
-                throw new NotImplementedException();
-            }
-
-            public T GetService<T>()
-            {
-                throw new NotImplementedException();
-            }
-
-            public T GetService<T>(string name)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void SetService(string name, object obj)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void SetService(object obj)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void RemoveService(Type type)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void RemoveService(string name)
-            {
-                throw new NotImplementedException();
-            }
-
-            public string Name { get; set; }
-            public ICalendarObject Parent { get; set; }
-            public ICalendarObjectList<ICalendarObject> Children { get; private set; }
-            public IICalendar Calendar { get; private set; }
-            public IICalendar iCalendar { get; private set; }
-            public int Line { get; set; }
-            public int Column { get; set; }
-            public ICalendarPropertyList Properties { get; private set; }
-            public event EventHandler<ObjectEventArgs<string, string>> UIDChanged;
-            public string UID { get; set; }
-            public IList<IAttendee> Attendees { get; set; }
-            public IList<string> Comments { get; set; }
-            public IDateTime DTStamp { get; set; }
-            public IOrganizer Organizer { get; set; }
-            public IList<IRequestStatus> RequestStatuses { get; set; }
-            public Uri Url { get; set; }
-            public void ClearEvaluation()
-            {
-                throw new NotImplementedException();
-            }
-
-            public IList<Occurrence> GetOccurrences(IDateTime dt)
-            {
-                throw new NotImplementedException();
-            }
-
-            public IList<Occurrence> GetOccurrences(DateTime dt)
-            {
-                throw new NotImplementedException();
-            }
-
-            public IList<Occurrence> GetOccurrences(IDateTime startTime, IDateTime endTime)
-            {
-                throw new NotImplementedException();
-            }
-
-            public IList<Occurrence> GetOccurrences(DateTime startTime, DateTime endTime)
-            {
-                throw new NotImplementedException();
-            }
-
-            public IDateTime DTStart { get; set; }
-            public IDateTime Start { get; set; }
-            public IList<IPeriodList> ExceptionDates { get; set; }
-            public IList<IRecurrencePattern> ExceptionRules { get; set; }
-            public IList<IPeriodList> RecurrenceDates { get; set; }
-            public IList<IRecurrencePattern> RecurrenceRules { get; set; }
-            public IDateTime RecurrenceID { get; set; }
-            public ICalendarObjectList<IAlarm> Alarms { get; private set; }
-            public IList<AlarmOccurrence> PollAlarms(IDateTime startTime, IDateTime endTime)
-            {
-                throw new NotImplementedException();
-            }
-
-            public IList<IAttachment> Attachments { get; set; }
-            public IList<string> Categories { get; set; }
-            public string Class { get; set; }
-            public IList<string> Contacts { get; set; }
-            public IDateTime Created { get; set; }
-            public string Description { get; set; }
-            public IDateTime LastModified { get; set; }
-            public int Priority { get; set; }
-            public IList<string> RelatedComponents { get; set; }
-            public int Sequence { get; set; }
-            public string Summary { get; set; }
-            public IDateTime DTEnd { get; set; }
-            public TimeSpan Duration { get; set; }
-            public IDateTime End { get; set; }
-            public bool IsAllDay { get; set; }
-            public IGeographicLocation GeographicLocation { get; set; }
-            public string Location { get; set; }
-            public IList<string> Resources { get; set; }
-            public EventStatus Status { get; set; }
-            public string WorkflowState { get; set; }
-            public TransparencyType Transparency { get; set; }
-            public bool IsActive()
-            {
-                throw new NotImplementedException();
+                var appointment = new Appointment(exchangeService);
+                foreach (var attendee in e.Attendees)
+                {
+                    var a = new Attendee(attendee.CommonName, attendee.Value.Authority);
+                    switch (attendee.Role)
+                    {
+                        case "REQ-PARTICIPANT":
+                            appointment.RequiredAttendees.Add(a);
+                            break;
+                        case "OPT-PARTICIPANT":
+                            appointment.OptionalAttendees.Add(a);
+                            break;
+                    }
+                }
+                appointment.Location = e.Location;
+                appointment.Categories = new StringList(e.Categories);
+                appointment.Subject = e.Summary;
+                appointment.Organizer.Name = e.Organizer.CommonName;
+                appointment.Organizer.Address = e.Organizer.Value.Authority;
+                appointment.Start = e.Start.Date;
+                appointment.StartTimeZone = TimeZoneInfo.FromSerializedString(e.Start.TimeZoneName);
+                appointment.End = e.End.Date;
+                appointment.EndTimeZone = TimeZoneInfo.FromSerializedString(e.End.TimeZoneName);
+                appointment.IsAllDayEvent = e.IsAllDay;
+                appointment.Body = new TextBody(e.Description);
+                appointment.ICalUid = e.UID;
+                return appointment;
             }
         }
 
         private readonly IRemoteCalendarAuthDataDO _authData;
         private readonly Lazy<ExchangeService> _lazyExchangeService;
+        private readonly EventToAppointmentAdapter _adapter;
 
         public EWSClient(IRemoteCalendarAuthDataDO authData)
         {
             _authData = authData;
             _lazyExchangeService = new Lazy<ExchangeService>(CreateServiceClient);
+            _adapter = new EventToAppointmentAdapter();
         }
 
         private ExchangeService CreateServiceClient()
@@ -226,12 +118,14 @@ namespace KwasantCore.Managers.APIManagers.Packagers.RemoteCalendar.EWS
                 new CalendarView(
                     TimeZoneInfo.ConvertTime(from, ExchangeService.TimeZone).DateTime,
                     TimeZoneInfo.ConvertTime(to, ExchangeService.TimeZone).DateTime));
-            return appointments.Select(a => new EWSEvent(a)).ToArray();
+            return appointments.Select(a => _adapter.AdaptAppointment(a)).ToArray();
         }
 
         public async Task CreateEventAsync(IRemoteCalendarLinkDO calendarLink, IEvent calendarEvent)
         {
-            throw new NotImplementedException();
+            var a = _adapter.AdaptEvent(calendarEvent, ExchangeService);
+            var folderId = new FolderId(calendarLink.RemoteCalendarHref);
+            a.Save(folderId);
         }
 
         public async Task<IDictionary<string, string>> GetCalendarsAsync(IRemoteCalendarAuthDataDO authData)
